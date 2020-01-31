ac41d62876c5fdb24f7e512335c45ae2f91ad33c
Adria TImm <atimm13@winona.edu>
2020-01-30 21:17:23 -0600

    Initial Commit

---
+++ TrialOneMillion/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TrialOneMillion
+//
+//  Created by Timm, Adria M on 1/30/20.
+//  Copyright © 2020 Timm, Adria M. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TrialOneMillion/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TrialOneMillion
+//
+//  Created by Timm, Adria M on 1/30/20.
+//  Copyright © 2020 Timm, Adria M. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
